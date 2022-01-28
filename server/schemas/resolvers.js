16a44a32d0e0f5f46cf7491219c187e01a881620
PFZM <pfzm@hotmail.com>
2022-01-28 21:46:11 +1100

    resolvers: addUser and addMessage

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,22 +4,25 @@ const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
-    users: async () => {
+    users: async ({}) => {
       return User.find().populate("messages");
     },
-    user: async (root, { _id }) => {
+    user: async (_, { _id }) => {
       return User.findOne({ _id }).populate("messages");
     },
-    me: async (root, args, context) => {
+    me: async (_, __, context) => {
       if (context.user) {
         return User.findOne({ _id: context.user._id }).populate("messages");
       }
       throw new AuthenticationError("You need to be logged in!");
     },
+    messages: async ({}) => {
+      return Message.find({}).sort({ createdAt: -1 });
+    },
   },
 
   Mutation: {
-    login: async (root, { email, password }) => {
+    login: async (_, { email, password }) => {
       try {
         const user = await User.findOne({ email });
         if (!user) {
@@ -40,7 +43,7 @@ const resolvers = {
       }
     },
 
-    signUp: async (root, { email, password }) => {
+    signUp: async (_, { email, password }) => {
       try {
         const signUpUser = await User.findOneAndUpdate(
           { email: email },
@@ -54,10 +57,53 @@ const resolvers = {
       }
     },
 
-    addUser: async (root, userDetails) => {
+    addUser: async (_, userDetails) => {
       try {
         const user = await User.create(userDetails);
         return { user };
+      } catch (err) {
+        console.error(err);
+      }
+    },
+
+    addMessage: async (_, { messageText }, context) => {
+      try {
+        if (context.user) {
+          const newMessage = await Message.create(
+            { messageText },
+            { status: "pending" }
+          );
+
+          await User.findOneAndUpdate(
+            { _id: context.user._id },
+            { $addToSet: { messages: newMessage._id } }
+          );
+
+          return newMessage;
+        }
+        throw new AuthenticationError("You need to be logged in!");
+      } catch (err) {
+        console.error(err);
+      }
+    },
+
+    addAnswer: async (_, { messageId, answerBody, answerId }, context) => {
+      try {
+        if (context.user) {
+          const answerMessage = await Answer.create(
+            { answerId },
+            { answerBody },
+            { userId: context.user._id }
+          );
+
+          await Message.findOneAndUpdate(
+            { _id: messageId },
+            { $push: answerMessage }
+          );
+
+          return answerMessage;
+        }
+        throw new AuthenticationError("You need to be logged in!");
       } catch (err) {
         console.error(err);
       }
